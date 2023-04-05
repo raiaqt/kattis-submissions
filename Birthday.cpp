97cc0eb39fa522a1dc3c30536d41456a404fcf6e
Raia Quitoriano <rsquitoriano@gmail.com>
2023-04-05 08:55:36 +0800

    bridges of a graph

--- Birthday.cpp
+++ Birthday.cpp
@@ -5,63 +5,60 @@ bool visited[100];
 bool adj[100][100];
 int id[100];
 int low[100];
+int parent[100];
 int count;
+bool hasBridge;
 
-void dfs(int curr, int parent) {
-    visited[curr] = true;
-    id[curr] = count++;
-    low[curr] = 2 * P;
-    for (int i = 0; i < P; i++) {
-        if (adj[curr][i]) {
-            if (!visited[i]) {
-                dfs(i, curr);
-                low[curr] = low[i] < low[curr] ? low[i] : low[curr];
-            } else if (i != parent) {   // has back edge
-               low[curr] = id[i] < low[curr] ? id[i] : low[curr];
-            }
-        }
-    }
-}
+void dfs(int u) {
+    visited[u] = true;
+    id[u] = count++;
+    low[u] = id[u];
 
-bool hasBridge() {
-    bool result = false;
-    for (int i = 1; i < P; i++) {
-        if (low[i] == 2 * P) {
-            return true;
-        }
+    for (int v = 0; v < P; v++) {
+    	if (adj[u][v]) {
+    		if (!visited[v]) {
+        		parent[v] = u;
+        		dfs(v);
+
+        		if (id[u] < low[v]) {
+        			hasBridge = true;
+        		}
+    		}
+        	if (parent[u] != v) {
+        		if (low[v] < low[u]) {
+        			low[u] = low[v];
+        		}
+        	}
+    	}
     }
-    return false;
 }
 
 int main() {
     scanf("%d %d", &P, &C);
 
     while (P + C > 0) {
-        // if (P == 1 && C == 0) {
-        //     printf("No\n");
-        //     scanf("%d %d", &P, &C);
-        //     continue;
-        // }
         for (int i = 0; i < P; i++) {
             visited[i] = false;
             low[i] = -1;
             id[i] = -1;
-            
+            parent[i] = -1;
+
             for (int j = 0; j < P; j++) {
                 adj[i][j] = false;
             }
         }
         count = 0;
+        hasBridge = false;
         int a, b;
         for (int i = 0; i < C; i++) {
             scanf("%d %d", &a, &b);
             adj[a][b] = true;
             adj[b][a] = true;
         }
-        
-        dfs(0, -1);
-        
-        printf("%s\n", count < P || hasBridge() ? "Yes" : "No");
+
+        dfs(0);
+
+        printf("%s\n", count < P || hasBridge ? "Yes" : "No");
         scanf("%d %d", &P, &C);
     }
     return 0;
